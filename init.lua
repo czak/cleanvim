7fca4875a3b4b5f535b29f664bdd282b5306358f
Łukasz Adamczak <14021+czak@users.noreply.github.com>
2025-10-19 22:46:35 +0200

    Tweak tree mappings

--- init.lua
+++ init.lua
@@ -289,9 +289,11 @@ require("nvim-tree").setup({
     end
 
     -- See :help nvim-tree-mappings-default
+    vim.keymap.set("n", "<C-]>", api.tree.change_root_to_node, opts("CD"))
     vim.keymap.set("n", "<C-t>", api.node.open.tab, opts("Open: New Tab"))
     vim.keymap.set("n", "<C-v>", api.node.open.vertical, opts("Open: Vertical Split"))
     vim.keymap.set("n", "<C-x>", api.node.open.horizontal, opts("Open: Horizontal Split"))
+    vim.keymap.set("n", ";", function(node) api.node.open.preview(node, { focus = true }) end, opts("Preview"))
     vim.keymap.set("n", "<BS>", api.node.navigate.parent_close, opts("Close Directory"))
     vim.keymap.set("n", "<CR>", api.node.open.edit, opts("Open"))
     vim.keymap.set("n", "-", api.tree.change_root_to_parent, opts("Up"))
