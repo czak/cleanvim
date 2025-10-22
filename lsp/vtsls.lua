2096b24c5b27408dce36117f4a47155a3c786720
Łukasz Adamczak <14021+czak@users.noreply.github.com>
2025-10-22 18:09:11 +0200

    vtsls: increase tsserver memory limit

--- lsp/vtsls.lua
+++ lsp/vtsls.lua
@@ -2,4 +2,11 @@ return {
   cmd = { 'vtsls', '--stdio' },
   filetypes = { 'javascript', 'javascriptreact', 'javascript.jsx', 'typescript', 'typescriptreact', 'typescript.tsx' },
   root_markers = { 'tsconfig.json', 'package.json', 'jsconfig.json', '.git' },
+  settings = {
+    typescript = {
+      tsserver = {
+        maxTsServerMemory = 8192,
+      },
+    },
+  },
 }
